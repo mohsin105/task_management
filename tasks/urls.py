706ed00109f2006d2a93967c44293fa1096aa581
Mohsin <mohsinibnaftab@gmail.com>
2025-02-06 20:14:49 +0600

    after module 4 tailwind integration done in module_4 branch

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from tasks.views import dashboard
+from tasks.views import dashboard,user_dashboard,manager_dashboard
 urlpatterns = [
-    path('dashboard/',dashboard)
+    path('dashboard/',dashboard),
+    path('user-dashboard/',user_dashboard),
+    path('manager-dashboard/',manager_dashboard)
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return HttpResponse("This is the task management website home page!")
+    # return HttpResponse("This is the task management website home page!")
+    return render(request,'home.html')
 
 def dashboard(request):
-    return HttpResponse('This is the dashboard page!')
+    return render(request,'dashboard/dashboard.html')
+
+def user_dashboard(request):
+    return render(request,'dashboard/user_dashboard.html')
+
+def manager_dashboard(request):
+    return render(request,'dashboard/manager_dashboard.html')
